82f431126c2a705c0954142e640030f7d2f82d92
Allison Basore <allisonbasore@Allisons-Air-4.home>
2020-03-19 12:45:28 -0400

    added solver function and tests

--- Solver.py
+++ Solver.py
@@ -1,29 +1,121 @@
+from classes import *
+import sys
+
+def Solve(table, matches, id=0):
+
+    #Base Cases
+    if(len(table)==0):
+        return True
+    if(len(matches)==0):
+        return False
+
+    print("Table ****** " + str(id))
+    for v in table:
+        v.print_card()
+
+    #Take off one match
+    firstMatch = matches[0]
+    matches = updateMatches(matches, firstMatch)
+    tableRemoved = updateTable(table, firstMatch)
+
+    id+=1
+    #Try either using the match or not using it
+    solutionA = Solve(tableRemoved, matches, id)
+    solutionB = Solve(table, matches, id)
+
+    if(solutionA or solutionB):
+        return True
+    else:
+        return False
+
+def updateTable(table, match):
+    newTable = []
+    idsToRemove = []
+    for card in match:
+        idsToRemove.append(card.id)
+    for card in table:
+        if(not card.id in idsToRemove):
+            newTable.append(card)
+    return newTable
+
+def hasMatchingCards(m1, m2):
+    for c1 in m1:
+        for c2 in m2:
+            if(c1.id == c2.id):
+                return True
+    return False
+
+def updateMatches(matches, match):
+    newMatches = []
+    for rawMatch in matches:
+        if(not hasMatchingCards(rawMatch, match)):
+            newMatches.append(rawMatch)
+    return newMatches
+
+
 def matches(table):
     matches = []
     for i in range(len(table)):
         for j in range(1,len(table)):
             for k in range(2,len(table)):
-                A = isCardMatch(table[i],table[j],table[k])
-                if A:
-                    matches.append([table[i],table[j],table[k]])
+                if(i!=j and i!=k and j!=k):
+                    if isCardMatch(table[i],table[j],table[k]):
+                        matches.append([table[i],table[j],table[k]])
     return matches
 
 def isCardMatch(c1,c2,c3):
+    """
+    Takes in three card objects, return a boolean value
+
+    Returns True if all three cards "Match" along all four dimensions (color, number, shape, shade)
+    Return False if any of the dimensions of the three cards do not match
+
+    A card is "Matched" if all three cards are either all the same or all unique
+    """
+    colorMatched = False
+    numberMatched = False
+    shapeMatched = False
+    shadeMatched = False
+
+    #If all the colors are the same or all arent the same, colors are Matched
     if c1.color == c2.color == c3.color:
-        return True
-    elif c1.color != c2.color != c3.color:
-        return True
-    elif c1.shape == c2.shape == c3.shape:
-        return True
-    elif c1.shape != c2.shape != c3.shape:
-        return True
-    elif c1.number == c2.number == c3.number:
-        return True
-    elif c1.number != c2.number != c3.number:
-        return True
-    elif c1.shape == c2.shape == c3.shape:
-        return True
-    elif c1.shape != c2.shape != c3.shape:
+        colorMatched = True
+    if (c1.color != c2.color) and (c1.color != c3.color) and (c2.color != c3.color):
+        colorMatched = True
+
+    #If all the numbers are the same or all arent the same, numbers are Matched
+    if c1.number == c2.number == c3.number:
+        numberMatched = True
+    if (c1.number != c2.number) and (c1.number != c3.number) and (c2.number != c3.number):
+        numberMatched = True
+
+    #If all the shape are the same or all arent the same, shape are Matched
+    if c1.shape == c2.shape == c3.shape:
+        shapeMatched = True
+    if (c1.shape != c2.shape) and (c1.shape != c3.shape) and (c2.shape != c3.shape):
+        shapeMatched = True
+
+    #If all the shade are the same or all arent the same, shade are Matched
+    if c1.shade == c2.shade == c3.shade:
+        shadeMatched = True
+    if (c1.shade != c2.shade) and (c1.shade != c3.shade) and (c2.shade != c3.shade):
+        shadeMatched = True
+
+    #If all are dimensions match, then this is a match
+    if( colorMatched and numberMatched and shapeMatched and shadeMatched):
         return True
     else:
         return False
+
+if __name__ == '__main__':
+    x=deck()
+    x.add_cards()
+    if(len(sys.argv) > 1):
+        testFile = sys.argv[1]
+        x.makeTableFromFile(testFile)
+    else:
+        x.make_default_table()
+    print("Deck Created and Table Set")
+    matches = matches(x.table)
+    solveable = Solve(x.table, matches)
+    print("Finished! The table is " + str(solveable))
--- classes.py
+++ classes.py
@@ -1,26 +1,67 @@
 import random
+from Solver import *
+
 class deck:
     def __init__(self):
         self.cards=[]
         self.table=[]
     def add_cards(self):
+        id=0
         for i in ("green","purple","red"):
             for j in ("oval","diamond","squiggle"):
-                for k in ("1","2","3"):
-                    for l in ("solid","stripped","outline"):
-                        self.cards.append(card(i,j,k,l))
+                for l in ("solid","stripped","outline"):
+                    for k in ("1","2","3"):
+                        self.cards.append(card(i,j,l,k,id))
+                        id+=1
 
-    def make_table(self):
-        for i in range (0,11):
+    def make_default_table(self):
+        for i in range (0,12):
             y=random.choice(self.cards)
             self.table.append(y)
+
+    def makeTableFromFile(self, testFile):
+        f = open(testFile, "r")
+        lines = [line.split() for line in f.readlines()]
+        f.close()
+
+        cards = []
+
+        lines = lines[1:]
+        idVal = 0
+        for line in lines:
+            if(len(line)):
+                if(line[0] == 'p'):
+                    self.numCards= line[1]
+                if(line[0] not in ('c', 'p') ):
+                    color = line[0]
+                    shape = line[1]
+                    shade = line[2]
+                    number = line[3]
+                    id = idVal
+                    idVal+=1
+                    self.table.append(card(color,shape,shade,number,id))
+        return
+
+
 class card:
-    def __init__(self,color,shape,shade,number):
+    def __init__(self,color,shape,shade,number,id):
         self.color=color
         self.shape=shape
         self.shade=shade
         self.number=number
-x=deck()
-x.add_cards()
-x.make_table()
-print(x.table)
+        self.id = id
+
+    def print_card(self):
+        print(str(self.id) + " *** " + self.color + " " + self.shape + " " + self.shade + " " + self.number )
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    x=deck()
+    x.add_cards()
+    if(len(filename) == 0):
+        x.make_default_table()
+    matches = matches(x.table)
+    for m in matches:
+        for x in m:
+            x.print_card()
+    print("Deck Created and Table Set")
